3dae6aa1eaa08a0735cccf737e4c1c1ff2134f5d
ao627515 <ao627515@gmail.com>
2025-04-08 06:21:00 +0000

    `Added error message display for email and password fields in signup form and updated input type for email field to email`

--- resources/views/pages/auth/signup.blade.php
+++ resources/views/pages/auth/signup.blade.php
@@ -3,19 +3,25 @@
 @section('content')
     <h1>Inscription</h1>
     <div class="todo-card login-card">
-        <form action="{{ route('signin') }}" method="post">
+        <form action="{{ route('signup') }}" method="post">
             @csrf
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="text" id="email" name="email" required>
+                <input type="email" id="email" name="email" required>
+                @error('email')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="password">Mot de passe</label>
                 <input type="password" id="password" name="password" required>
+                @error('password')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
             </div>
             <div class="form-group">
-                <label for="password-confirm">Confirmer le Mot de passe</label>
-                <input type="password-confirm" id="password-confirm" name="password-confirm" required>
+                <label for="password_confirmation">Confirmer le Mot de passe</label>
+                <input type="password" id="password_confirmation" name="password_confirmation" required>
             </div>
             <div class="form-actions">
                 <button type="submit" class="btn-primary" id="signinBtn">S'inscrire</button>
