b7e2e5dd3642a5e3bcfe21ba56468901dd8e3fdd
ao627515 <ao627515@gmail.com>
2025-04-10 11:29:24 +0000

    add categories resource

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/todos/{todo}/toogle/completed', [ToDoController::class, 'toggleCompleted'])->name('todos.toggle.completed');
     Route::resource('todos', ToDoController::class)->except(['show', 'edit', 'create']);
     Route::post('signout', [AuthController::class, 'signout'])->name('signout');
+    Route::resource('categories', CategoryController::class);
 });
