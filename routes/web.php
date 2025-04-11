d6b88f7615050c4a9f5cd510d468b3e434548178
ao627515 <ao627515@gmail.com>
2025-04-11 13:24:30 +0000

    maj

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,3 @@ Route::middleware('auth')->group(function () {
     Route::post('signout', [AuthController::class, 'signout'])->name('signout');
     Route::resource('categories', CategoryController::class)->except(['show', 'create', 'edit']);
 });
-
-Route::middleware('auth')->group(function () {});
